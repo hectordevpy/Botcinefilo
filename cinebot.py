e00c9661b58772460aae3a60c0db99a44ff41629
hectordev <hsoto.doer@gmail.com>
2021-11-01 23:53:14 -0500

    Logrando que @botdelcine retuitee hashtag sobre cine

--- cinebot.py
+++ cinebot.py
@@ -4,40 +4,61 @@ import time
 import imdb
 from dotenv import load_dotenv
 import os
+import random
 
-<<<<<<< HEAD
-consumer_key = os.getenv('consumer_key')
-consumer_secret = os.getenv('consumer_secret')
+
+#enviroment variables
+load_dotenv()
+
+API_KEY = os.getenv('API_key')
+API_SECRET = os.getenv('API_secret')
+
 
 key = os.getenv('key')
 secret = os.getenv('secret')
-=======
->>>>>>> a691813e790b36b14931743bb4cf6afd649107aa
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
 auth.set_access_token(key, secret)
 
 api= tweepy.API(auth, wait_on_rate_limit=True)
 
+#@botdelcine retweet topics
 
-def run():
+def retweet(hashtag,api):
+    for twit in tweepy.Cursor(api.search, q=(hashtag)).items(10):
+        try:
+            twit.retweet()
+            time.sleep(10)
+        except:
+            print("Can't retweet  "+str(twit.id)+" by user"+twit.user.screen_name)
 
-    moviesDB = imdb.IMDb()
-
-    #get list of the best movies
-    top = moviesDB.get_top250_movies()
-
-    print('Top 100 movies: ')
-    for movie in top[0:101]:
-        title = movie["title"]
-        year = movie["year"]
-        rating = movie["rating"]
-        #directors = movie["directors"]
-        print(f"🎥{title} - {year}")
-        print(f"🌟Rating: {rating}")
-        # direcStr = " ".join(map(str,directors))
-        # print(f"directors: {direcStr}") 
+subjects = ["Cineperuano", "Oscars2022", "CineLatinoamericano", "Sitges2021", "LeónDeOro", "Cannes2021"]
+for subject in subjects:
+    retweet("#"+subject,api)
 
 
-if __name__ == "__main__":
-    run()
+#     moviesDB = imdb.IMDb()
+
+#     # get list of the best movies
+#     top = moviesDB.get_top250_movies()
+#     # k = number of movies to select
+#     top_3 = random.choices(top, k=3)
+#     print('Mis recomendaciones 🤓: ')
+#     for movie in top_3:
+#         title = movie["title"]
+#         year = movie["year"]
+#         rating = movie["rating"]
+    
+#         print(f"🎥{title} - {year}")
+#         print(f"🌟Rating: {rating}")
+
+# def run():
+    
+#     while True:
+#         try:
+#             schedule.run_pending()
+#         except tweepy.tweepyError as e:
+#             raise e
+    
+# if __name__ == "__main__":
+#     run()
